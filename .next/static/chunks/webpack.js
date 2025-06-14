a8d753cd4fa104cb3489da6a5d58ec8cf261fc3d
VictorOblate <victoroblate@gmail.com>
2025-06-14 14:17:09 +0000

    Lint Error fix

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "0236af5a12fa1eb8"; }
+/******/ 		__webpack_require__.h = function() { return "ca28145ff9ba43a9"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
